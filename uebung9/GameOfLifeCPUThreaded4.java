afa94c1d17f675d861010068ba0977522de4c6f3
DiddiZ <robbzkupper@web.de>
2016-02-03 00:13:02 +0100

    Tweaked Game of Life by adding a discrete worker for the edges;

--- uebung9/GameOfLifeCPUThreaded4.java
+++ uebung9/GameOfLifeCPUThreaded4.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author Robin Kupper
  */
-public class GameOfLifeCPUThreaded3 extends GameOfLifeCPU
+public class GameOfLifeCPUThreaded4 extends GameOfLifeCPU
 {
 	private byte[] open, tempOpen;
 
@@ -17,7 +17,7 @@ public class GameOfLifeCPUThreaded3 extends GameOfLifeCPU
 	private static final int CHUNKS_X = 16, CHUNKS_Y = 16;
 	private static final int CHUNK_WIDTH = WIDTH / CHUNKS_X, CHUNK_HEIGHT = HEIGHT / CHUNKS_Y;
 
-	public GameOfLifeCPUThreaded3() {
+	public GameOfLifeCPUThreaded4() {
 		temp = createArray();
 		open = createArray();
 		tempOpen = createArray();
@@ -28,10 +28,15 @@ public class GameOfLifeCPUThreaded3 extends GameOfLifeCPU
 		try {
 			// Spawn a lot of threads
 			final ExecutorService executor = Executors.newWorkStealingPool();
+			executor.submit(new EdgeWorker());
 			for (int x = 0; x < CHUNKS_X; x++)
-				for (int y = 0; y < CHUNKS_Y; y++)
-					executor.submit(new Worker(x * CHUNK_WIDTH, (x + 1) * CHUNK_WIDTH, y * CHUNK_HEIGHT, (y + 1) * CHUNK_HEIGHT));
-
+				for (int y = 0; y < CHUNKS_Y; y++) {
+					final int minX = x == 0 ? 1 : x * CHUNK_WIDTH;
+					final int maxX = x == CHUNKS_X - 1 ? WIDTH - 1 : (x + 1) * CHUNK_WIDTH;
+					final int minY = y == 0 ? 1 : y * CHUNK_HEIGHT;
+					final int maxY = y == CHUNKS_Y - 1 ? HEIGHT - 1 : (y + 1) * CHUNK_HEIGHT;
+					executor.submit(new NoEdgeWorker(minX, maxX, minY, maxY));
+				}
 			// Wait for spawned threads to finish
 			executor.shutdown();
 			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
@@ -124,22 +129,17 @@ public class GameOfLifeCPUThreaded3 extends GameOfLifeCPU
 		}
 	}
 
-	private class Worker implements Runnable
+	/**
+	 * Processes the edge
+	 */
+	private class EdgeWorker implements Runnable
 	{
-		private final int minX, maxX, minY, maxY;
-
-		public Worker(int minX, int maxX, int minY, int maxY) {
-			this.minX = minX;
-			this.maxX = maxX;
-			this.minY = minY;
-			this.maxY = maxY;
-		}
-
 		@Override
 		public void run() {
 			int idx;
-			for (int y = minY; y < maxY; y++)
-				for (int x = minX; x < maxX; x++) {
+			for (int y = 0; y < HEIGHT; y++) {
+				final int increment = y == 0 || y == HEIGHT - 1 ? 1 : WIDTH - 1; // Skip all non-edge cells
+				for (int x = 0; x < WIDTH; x += increment) {
 					idx = y * WIDTH + x;
 					if (open[idx] == tick) { // Cell changed in last step
 						final int activeNeighbors = getNumberOfActiveNeighbors(y, x);
@@ -153,6 +153,66 @@ public class GameOfLifeCPUThreaded3 extends GameOfLifeCPU
 							addToOpen(y, x);
 					}
 				}
+			}
+		}
+	}
+
+	/**
+	 * Assumes neighbors are not edges
+	 */
+	private class NoEdgeWorker implements Runnable
+	{
+		private final int minX, maxX, minY, maxY;
+
+		public NoEdgeWorker(int minX, int maxX, int minY, int maxY) {
+			this.minX = minX;
+			this.maxX = maxX;
+			this.minY = minY;
+			this.maxY = maxY;
+		}
+
+		@Override
+		public void run() {
+			for (int y = minY; y < maxY; y++) {
+				final int maxIdx = y * WIDTH + maxX;
+				for (int idx = y * WIDTH + minX; idx < maxIdx; idx++)
+					if (open[idx] == tick) { // Cell changed in last step
+						final int activeNeighbors = getNumberOfActiveNeighborsNoEdge(idx);
+						final boolean wasActive = current[idx] != 0;
+
+						// A Cell is active if it has three neighbors, or has two neighbors and is already active
+						final boolean isActive = activeNeighbors == 3 || wasActive && activeNeighbors == 2;
+						temp[idx] = isActive ? (byte)1 : 0;
+
+						if (isActive != wasActive)
+							addToOpenNoEdge(idx);
+					}
+			}
+		}
+
+		protected int getNumberOfActiveNeighborsNoEdge(int idx) {
+			// Sum all neighbors to get the count of active neighbors
+			return current[idx - WIDTH - 1] +
+					current[idx - WIDTH] +
+					current[idx - WIDTH + 1] +
+					current[idx - 1] +
+					current[idx + 1] +
+					current[idx + WIDTH - 1] +
+					current[idx + WIDTH] +
+					current[idx + WIDTH + 1];
+		}
+
+		private void addToOpenNoEdge(int idx) {
+			final byte nextTick = (byte)(tick + 1);
+			tempOpen[idx - WIDTH - 1] = nextTick;
+			tempOpen[idx - WIDTH] = nextTick;
+			tempOpen[idx - WIDTH + 1] = nextTick;
+			tempOpen[idx - 1] = nextTick;
+			tempOpen[idx] = nextTick;
+			tempOpen[idx + 1] = nextTick;
+			tempOpen[idx + WIDTH - 1] = nextTick;
+			tempOpen[idx + WIDTH] = nextTick;
+			tempOpen[idx + WIDTH + 1] = nextTick;
 		}
 	}
 }
